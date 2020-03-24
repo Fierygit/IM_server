554afee90afb826c36de49464b7284bb3eaafa77
mingor <ming@947462457.com>
2020-03-24 17:38:48 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# 文明——IM服务器
+### 项目介绍
+
+本项目是实现一个简单的聊天室，聊天室分为服务端和客户端。采用C/S模型，使用TCP连接, 使用epoll多路复用技术。
+
+### 使用
+```shell
+mkdir build
+cd build
+cmake ..
+make
+./IM_server
+```
+
---
+++ cmake-build-debug/CMakeFiles/IM_server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IM_server.dir/src/server.cpp.obj"
+  "IM_server.exe"
+  "IM_server.exe.manifest"
+  "IM_server.pdb"
+  "libIM_server.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IM_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
